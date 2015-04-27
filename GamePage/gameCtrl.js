23a12bdd12ff5e99e40a091c9f4b7c6c731fa7ec
whitneypage <wpagegarner@gmail.com>
2015-04-27 15:20:15 -0600

    Working

--- GamePage/gameCtrl.js
+++ GamePage/gameCtrl.js
@@ -2,33 +2,36 @@ var app = angular.module('mapApp');
 
 app.controller('gameCtrl', function($scope, gameService) {
 
-//$scope.coordinates = chance.coordinates().split(' ').join('');
-//alert($scope.coordinates);
-// $scope.coordinates = gameService.coordinates();
-// $scope.stateName = gameService.stateName();
+//$scope.coordinates = chance.coordinates().replace(/,/g, '').split(" ");
+//console.log($scope.coordinates);
+
+ //var lat = $scope.coordinates[0]  
+ //var long = $scope.coordinates[1]
+
  $scope.score = 0;
 
 
-$scope.map = { 
-	center: { latitude: 36.964645, longitude: -122.01523 }, 
+$scope.map = {
+	center: { 
+	  latitude: gameService.latitude, 
+	  longitude: gameService.longitude 
+	}, 
 	zoom: 18,
-};
+	};
+	
 
 $scope.options = {
-	mapTypeId: google.maps.MapTypeId.SATELLITE
-};
-
-
-
-
-
-
-
+	mapTypeId: google.maps.MapTypeId.SATELLITE,
+	mapTypeControl: false,
+	minZoom: 11,
+	maxZoom: 18
+	
+ };
 
 
 
 $scope.addScore = function() {	
-    if ($scope.userGuess.toLowerCase() === $scope.stateName){
+    if ($scope.userGuess.toLowerCase() === gameService.stateName){
 	    $scope.score += 100;
 	    alert("You got it right!")
     } else {
@@ -36,6 +39,8 @@ $scope.addScore = function() {
     }
   $scope.userGuess = "";
 }
+
+
 
 
 
--- GamePage/gameService.js
+++ GamePage/gameService.js
@@ -5,31 +5,29 @@ app.service('gameService', function($http, $q) {
 	var places = [ 
  {
  	name: 'texas',
- 	coordinates: '30.274299,-97.742249'
+ 	latitude: '30.274299',
+ 	longitude: '-97.742249'
  },
  {
 
    name: 'utah',
-   coordinates: '40.244626,-111.653326'
+   latitude: '40.777258',
+   longitude: '-111.888198'
  },
 ]
 
 
-var localObj = {};
-this.coordinates = function() {
+
+
     for (var i = 0; i < places.length; i++) {
-        localObj = places[i];
-        return localObj.coordinates;
+    	this.stateName = places[i].name;
+        this.latitude = places[i].latitude;
+        this.longitude = places[i].longitude;
+
     }
-
-}
-
-
-this.stateName = function() {
-    for (var i = 0; i < places.length; i++) {
-       return places[i].name;
-   }
-};
+console.log(this.stateName);
+console.log(this.latitude);
+console.log(this.longitude);
 
 
 
@@ -51,4 +49,7 @@ this.stateName = function() {
 
 
 
-})
+
+
+
+}); 
